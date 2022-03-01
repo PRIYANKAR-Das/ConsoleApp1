e6a221639d9480512d8f7178d8ee974507e94f30
PRIYANKAR-Das <PRIYANKAR.Das@ibm.com>
2022-03-01 15:53:07 +0530

    calculator, natural numbers

---
+++ Sum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Sum
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program to print the sum of n natural numbers");
+
+            Console.WriteLine("n: ");
+
+            int n = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            for (int i = 1; i <= n; ++i)
+            {
+                sum += i;
+            }
+
+            Console.WriteLine("The sum of {0} natural numbers is {1}", n, sum);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ calculator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class calculator
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Basic Calculator");
+
+            Console.WriteLine("Enter first number: ");
+            double num1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter second number: ");
+            double num2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Specify the operation: +, -, *, /");
+            char operation = char.Parse(Console.ReadLine());
+
+            double result = -1;
+            
+            switch(operation)
+            {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    result = num1 / num2;
+                    break;
+                default:
+                    Console.WriteLine("Invalid Operation");
+                    break;
+            }
+
+            Console.WriteLine("{0} {1} {2} = {3}", num1, operation, num2, result);
+
+            Console.ReadLine();
+        }
+    }
+}
