b579cf4a72b9c492cb4ac60b38d44a4ef4b64408
PRIYANKAR-Das <PRIYANKAR.Das@ibm.com>
2022-03-02 11:29:05 +0530

    Param Array

---
+++ ParamArray.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class ParamArray
+    {
+        static void Main(string[] args)
+        {
+            //int[] list = new int[] { 1, 2, 3, 4 };
+            ParamArray obj = new ParamArray();
+            double avg = obj.Avg(1,2,3,4);
+            Console.WriteLine(avg);
+            Console.ReadKey();
+        }
+
+        double Avg(params int[] list)
+        {
+            double total = 0;
+            foreach (int num in list)
+            {
+                total += num;
+            }
+
+            return (total / list.Length);
+        }
+    }
+}
---
+++ PassArray.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class PassArray
+    {
+        static void Main(string[] args)
+        {
+            int[] list = new int[] {1,2,3,4};
+            PassArray obj = new PassArray();
+            double avg = obj.Avg(list);
+            Console.WriteLine(avg);
+            Console.ReadKey();
+        }
+
+        double Avg(int[] list)
+        {
+            double total = 0;
+            foreach (int num in list)
+            {
+                total += num;
+            }
+
+            return (total / list.Length);
+        }
+    }
+}
