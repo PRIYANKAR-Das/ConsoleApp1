ba21986330a993bd641f86817e9f11716620807b
PRIYANKAR-Das <PRIYANKAR.Das@ibm.com>
2022-03-03 09:09:48 +0530

    Various Methods on Collections

---
+++ Methods.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Collections;
+
+namespace ConsoleApp1
+{
+    internal class Methods
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList();
+            list.Add("PI");
+            list.Add(3.141);
+            list.Add(3);
+
+            foreach (object o in list)
+            {
+                Console.WriteLine(o);
+            }
+
+            list.RemoveAt(2);
+
+            foreach (object o in list)
+            {
+                Console.WriteLine(o);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
