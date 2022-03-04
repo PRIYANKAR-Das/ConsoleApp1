9f1781e27c87b7fb7ad20b4d8b6f4567d844ca8c
PRIYANKAR-Das <PRIYANKAR.Das@ibm.com>
2022-03-04 14:27:10 +0530

    Properties

---
+++ Properties.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Properties
+    {
+        class Student
+        {
+            private string name;
+            private int marks;
+            
+            public Student(string name, int marks)
+            {
+                this.name = name;
+                this.marks = marks;
+            }
+            public string Name
+            {
+                get { return name; }
+                set { name = value; }
+            }
+
+            public int Marks
+            {
+                get { return marks; }
+            }
+
+            public void print()
+            {
+                Console.WriteLine("Name:" + this.name);
+                Console.WriteLine("Marks:" + this.marks);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Student s1 = new Student("John", 85);
+
+            Console.ReadKey();
+
+            s1.print();
+
+            Console.ReadKey();
+
+            s1.Name = "Jack";
+            s1.print();
+
+            Console.ReadKey();
+            Console.WriteLine(s1.Marks);
+
+            Console.ReadKey();
+        }
+    }
+}
