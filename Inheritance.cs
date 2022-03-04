c060afdf89cb69de8262efbc75f844a499a54f5b
PRIYANKAR-Das <PRIYANKAR.Das@ibm.com>
2022-03-04 17:22:55 +0530

    Inheritance, need to work on access modifiers

---
+++ Inheritance.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Inheritance
+    {
+        class A
+        {
+            public int a = 1;
+        }
+
+        class B : A
+        {
+            public int b = 2;
+            public int a = 5;
+        }
+
+        class C : B
+        {
+            public int c = 3;
+            public int a = 4;
+
+            public void m()
+            {
+                Console.WriteLine(base.a);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            C obj = new C();
+            //Console.WriteLine(obj.m());
+            obj.m();
+
+            Console.ReadKey();
+        }
+    }
+}
