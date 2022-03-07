e3f5806d55eda3eb62d73f62f2fd53e90b6d9273
PRIYANKAR-Das <PRIYANKAR.Das@ibm.com>
2022-03-07 11:54:22 +0530

    Interface

---
+++ AbstractClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class AbstractClass
+    {
+        public abstract class A
+        {
+            public abstract void p();
+        }
+
+        public class B : A
+        {
+            public override void p()
+            {
+                Console.WriteLine("From B");
+            }
+        }
+
+        public class C : A
+        {
+            public override void p()
+            {
+                Console.WriteLine("From C");
+            }
+        }
+        static void Main(string[] args)
+        {
+            A o1 = new B();
+            A o2 = new C();
+
+            o1.p();
+            o2.p();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Interface.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Interface
+    {
+        interface IPrint
+        {
+            void Print();
+        }
+
+        class A : IPrint
+        {
+            public void Print()
+            {
+                Console.WriteLine("From A, IPrint");
+            }
+        }
+
+        interface IPrint2
+        {
+            void Print2();
+        }
+
+        class B : IPrint, IPrint2
+        {
+            public void Print()
+            {
+                Console.WriteLine("From B, IPrint");
+            }
+
+            public void Print2()
+            {
+                Console.WriteLine("From B, IPrint2");
+            }
+        }
+        static void Main(string[] args)
+        {
+            A obj1 = new A();
+            B obj2 = new B();
+            obj1.Print();
+            obj2.Print();
+            obj2.Print2();
+
+            Console.ReadKey();
+        }
+    }
+}
