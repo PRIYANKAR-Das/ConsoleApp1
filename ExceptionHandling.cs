bd7e362355a316d05f30f72dd1379966be3eb541
PRIYANKAR-Das <PRIYANKAR.Das@ibm.com>
2022-03-08 10:26:51 +0530

    User defined exceptions, various exception classes

---
+++ ExceptionHandling.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class ExceptionHandling
+    {
+        void m() { }
+        static void Main(string[] args)
+        {
+            int[] a = new int[3];
+
+            try
+            {
+                a[3] = 4;
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine("Error");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("finally");
+            }
+
+            ExceptionHandling o = null;
+            
+            try
+            {
+                o.m();
+            }
+            catch (NullReferenceException e)
+            {
+                Console.WriteLine("Error" + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("finally");
+            }
+
+            ExceptionHandling o2 = new ExceptionHandling();
+
+            string s = null;
+
+            try
+            {
+                "Calculate".IndexOf(s);
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("finally");
+            }
+
+            s = "String";
+
+            try
+            {
+                s.Substring(s.Length + 1);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("finally");
+            }
+
+            Console.ReadKey();
+        }
+
+        void k()
+        {
+            k();
+        }
+    }
+}
---
+++ UserDefinedException.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class UserDefinedException
+    {
+        class NonZeroException : Exception
+        {
+            public NonZeroException(string message)
+                : base(message)
+            {
+
+            }
+        }
+        class A
+        {
+            public int a;
+
+            public void m()
+            {
+                if (a > 0)
+                {
+                    throw (new NonZeroException("Non zero"));
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            A obj1 = new A();
+
+            obj1.a = 1;
+
+            try
+            {
+                obj1.m();
+            }
+            catch (NonZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("finally");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
