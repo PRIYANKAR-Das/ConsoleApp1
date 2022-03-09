1af0d49b11b5adfa79a85a3b2505c501b65cd998
PRIYANKAR-Das <PRIYANKAR.Das@ibm.com>
2022-03-09 16:12:49 +0530

    File handling

---
+++ File1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    internal class File1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Writing directory C:\class work");
+
+            Console.WriteLine("Enter file name: ");
+            string fileName = Console.ReadLine();
+            Console.WriteLine("Type the content");
+            string content = Console.ReadLine();
+            File.WriteAllText(@"C:\class work\" + fileName + ".txt", content);
+
+            Console.ReadKey();
+        }
+    }
+}
